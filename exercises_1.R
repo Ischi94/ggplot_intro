9dee6a0ac68412d412386f7466bf512baf50b9de
Ischi94 <gmathes94@gmx.de>
2020-04-11 12:05:48 +0200

    Added aesthetics. Started to work on the exercises file.

---
+++ exercises_1.R
@@ -0,0 +1,28 @@
+# exercises for the life coding session
+library(tidyverse)
+
+
+# Aesthetics --------------------------------------------------------------
+
+# In our first example, we mapped class to the color aesthetic, 
+# but we could have mapped class to the size aesthetic in the same way. 
+# In this case, the exact size of each point would reveal its class affiliation. 
+# Try to map class to the size aesthetic. Is it a good idea?
+###
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, colour = class))
+###
+
+# Or you could have mapped class to the alpha aesthetic, 
+# which controls the transparency of the points, 
+# or to the shape aesthetic, which controls the shape of the points. Try both. What 
+# happens to the symbol for SUVs?
+###
+
+###
+
+# What happens when you map a level to an aesthetic outsid of aes()?
+###
+
+###
+
